bf551fc494e500940e309ae6ad0a295dc5297d78
Gloryprod <gracia.ouinsou@gmail.com>
2026-09-14 00:58:13 +0100

    add db setup route

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use App\Http\Controllers\DiagnosticController;
+use Illuminate\Support\Facades\Artisan;
 
 Route::get('/', function () {
     return Inertia::render('introduction');
@@ -14,3 +15,14 @@ Route::post('/diagnostic/phase-1/{diagnostic?}', [DiagnosticController::class, '
 Route::get('/diagnostic/{diagnostic}/phase-2', [DiagnosticController::class, 'showPhase2'])->name('diagnostic.phase2');
 Route::post('/diagnostic/{diagnostic}/phase-2', [DiagnosticController::class, 'storePhase2'])->name('diagnostic.phase2.store');
 Route::get('/diagnostic/{diagnostic}/rapport', [DiagnosticController::class, 'showReport'])->name('diagnostic.report');
+
+Route::get('/setup-db-alodo', function () {
+    try {
+        Artisan::call('migrate:fresh', ['--force' => true]);
+        Artisan::call('config:clear');
+        Artisan::call('route:clear');
+        return "Base de données configurée et migrée avec succès !";
+    } catch (\Exception $e) {
+        return "Erreur lors de la migration : " . $e->getMessage();
+    }
+});
